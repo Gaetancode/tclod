09c51b931100d8bcd6bdb440c559ef82fd66a1b0
Gaetancode_ <gaetangiannone@gmail.com>
2024-11-19 23:42:10 +0100

    fix date issue 0

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,15 @@
 import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.string().transform((str) => {
+      const [day, month, year] = str.split('/');
+      return new Date(`${year}-${month}-${day}`).toISOString();
+    }),
+    heroImage: z.string().optional(),
+  }),
 });
 
 export const collections = { blog };
