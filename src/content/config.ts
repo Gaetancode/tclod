35f4af7ceec7c179eb3fcdf8caf4e27454b5c996
Gaetancode_ <gaetangiannone@gmail.com>
2024-11-19 23:54:20 +0100

    fix date issue 1

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,8 +5,14 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     pubDate: z.string().transform((str) => {
+      if (!str) return new Date();
       const [day, month, year] = str.split('/');
-      return new Date(`${year}-${month}-${day}`).toISOString();
+      return new Date(`${year}-${month}-${day}`);
+    }),
+    updatedDate: z.string().optional().transform((str) => {
+      if (!str) return undefined;
+      const [day, month, year] = str.split('/');
+      return new Date(`${year}-${month}-${day}`);
     }),
     heroImage: z.string().optional(),
   }),
